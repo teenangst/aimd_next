e016d53853ac746c8dc2c8a933766b643591f5c7
z- <30373155+teenangst@users.noreply.github.com>
2023-09-02 23:14:32 +0100

    Replaced bzip2 library
    Was getting erroneous errors caused by bzip2-rs

--- src/server_status.rs
+++ src/server_status.rs
@@ -1,7 +1,8 @@
-use std::{path::{PathBuf, Path}, fs::{self, File}, io};
+use std::{path::{PathBuf, Path}, fs::{self, File}, io::{self, Read, Write}, thread, time::Duration};
 
 use crate::{types::Server, files::has_map};
-use bzip2_rs::DecoderReader;
+use bzip2::read::BzDecoder;
+// use bzip2_rs::DecoderReader;
 use console::{style, StyledObject};
 
 fn fetch_servers() -> Result<Result<Vec<Server>, serde_json::Error>, reqwest::Error> {
@@ -36,55 +37,67 @@ fn get_uris(directory:&str, nextmap: &str) -> (String, String) {(
 )}
 
 fn check_nextmap(directory: &PathBuf, nextmap: &String, emojis: bool) -> Result<(), Box<dyn std::error::Error>> {
+  print!("{}", style(format!("Downloading {}.bsp.bz2... ", nextmap)).cyan());
+
+  let destination_directory = "temp";
+
+  if !Path::new(destination_directory).exists() {
+    fs::create_dir(destination_directory)?;
+  }
+
+  let (url, destination) = get_uris(destination_directory, nextmap);
+
+  let mut response = reqwest::blocking::get(url)?;
+  let mut file = fs::File::create(destination)?;
+
+  if let Some(content_length) = response.content_length() {
+    let content_length_mb = content_length as f64 / (1024.0 * 1024.0);
+    println!("{}", style(format!("{:.2} MB", content_length_mb)).cyan().dim());
+  } else {
+    println!("");
+  }
+
+  io::copy(&mut response, &mut file)?;
+
+  println!("{}", style(format!("Unzipping {}.bsp... ", nextmap)).cyan());
+  thread::sleep(Duration::from_secs(1));
+
+  let unzip_destination = directory.join(format!("{}.bsp", nextmap));
+  let compressed_file = File::open(format!("temp\\{}.bsp.bz2", nextmap))?;
+  let mut decompressor = BzDecoder::new(compressed_file);
+
+  let mut uncompressed_data = Vec::new();
+  decompressor.read_to_end(&mut uncompressed_data)?;
+
+  let mut unzipped_file = File::create(unzip_destination)?;
+  unzipped_file.write_all(&uncompressed_data)?;
+
+  Ok(())
+}
+
+fn do_check_nextmap(directory: &PathBuf, nextmap: &String, emojis: bool) {
   if has_map(directory, nextmap.clone()) {
     if emojis {
       println!("✅");
     }
-    Ok(())
   } else {
     if emojis {
       println!("❌");
     }
-    print!("{}", style(format!("Downloading {}.bsp.bz2... ", nextmap)).cyan());
+    match check_nextmap(directory, nextmap, emojis) {
+      Ok(_) => {
+          println!("{}", style("✅ Downloaded and unzipped successfully!").cyan());
+          let _ = fs::remove_file(format!("temp\\{}.bsp.bz2", nextmap));
+      },
+      Err(e) => {
+        println!("{}", style("❌ Download failed!").red());
+        eprintln!("\t{}", style(e).red());
 
-    let destination_directory = "temp";
+        let _ = fs::remove_file(format!("temp\\{}.bsp.bz2", nextmap));
 
-    if !Path::new(destination_directory).exists() {
-      fs::create_dir(destination_directory)?;
+        let _ = fs::remove_file(directory.join(format!("{}.bsp", nextmap)));
+      }
     }
-
-    let (url, destination) = get_uris(destination_directory, nextmap);
-
-    let mut response = reqwest::blocking::get(url)?;
-    let mut file = fs::File::create(destination)?;
-
-    if let Some(content_length) = response.content_length() {
-      let content_length_mb = content_length as f64 / (1024.0 * 1024.0);
-      println!("{}", style(format!("{:.2} MB", content_length_mb)).cyan().dim());
-    } else {
-      println!("");
-    }
-
-    io::copy(&mut response, &mut file)?;
-
-    print!("{}", style(format!("Unzipping {}.bsp... ", nextmap)).cyan());
-
-    let unzip_destination = directory.join(format!("{}.bsp... ", nextmap));
-    let compressed_file = File::open(format!("temp\\{}.bsp.bz2", nextmap))?;
-    let mut decompressed_output = File::create(unzip_destination)?;
-
-    let mut reader = DecoderReader::new(compressed_file);
-    io::copy(&mut reader, &mut decompressed_output)?;
-
-    fs::remove_file(format!("temp\\{}.bsp.bz2", nextmap))?;
-
-    if has_map(directory, nextmap.clone()) {
-      println!("{}", style("✅ Downloaded and unzipped successfully!").cyan());
-    } else {
-      println!("{}", style("❌ Download failed!").red());
-    }
-
-    Ok(())
   }
 }
 
@@ -94,8 +107,10 @@ fn parse_server(directory: &PathBuf, server:&Server) {
       match &server.info {
         Some(info) => {
           print!("{}:{} [{}] {} -> {} ", &server.address, server.port, colourise_players(info.players, info.max_players), info.map, nextmap);
-          let _ = check_nextmap(directory, nextmap, true);
-          let _ = check_nextmap(directory, &info.map, false);
+          // let _ = check_nextmap(directory, nextmap, true);
+          // let _ = check_nextmap(directory, &info.map, false);
+          do_check_nextmap(directory, nextmap, true);
+          do_check_nextmap(directory, &info.map, false);
         },
         None => {
           print!("{}:{} [?/?] ? -> {} ", &server.address, server.port, nextmap);
